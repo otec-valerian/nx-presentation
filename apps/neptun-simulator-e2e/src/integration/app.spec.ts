01dd655136d307b3ffdff107a537add882ce1f40
mykyta.chechet <chechet2002@gmail.com>
2022-05-19 17:03:41 +0300

    start2

---
+++ apps/neptun-simulator-e2e/src/integration/app.spec.ts
@@ -0,0 +1,13 @@
+import { getGreeting } from '../support/app.po';
+
+describe('neptun-simulator', () => {
+  beforeEach(() => cy.visit('/'));
+
+  it('should display welcome message', () => {
+    // Custom command example, see `../support/commands.ts` file
+    cy.login('my-email@something.com', 'myPassword');
+
+    // Function helper example, see `../support/app.po.ts` file
+    getGreeting().contains('Welcome neptun-simulator');
+  });
+});
---
+++ apps/neptun-simulator/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-presentation-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'neptun-simulator';
+}
